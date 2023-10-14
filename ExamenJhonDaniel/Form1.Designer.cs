51eca154239d1b2adbe380f508c7166284929055
Daniel04r <Jondanieljon26@hotmail.com>
2023-10-14 13:27:30 -0400

    Agregar archivos de proyecto.

---
+++ ExamenJhonDaniel/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ExamenJhonDaniel
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtId = new TextBox();
+            txtnombre = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            btnInsertar = new Button();
+            SuspendLayout();
+            // 
+            // txtId
+            // 
+            txtId.Location = new Point(97, 165);
+            txtId.Name = "txtId";
+            txtId.Size = new Size(125, 27);
+            txtId.TabIndex = 0;
+            txtId.TextChanged += textBox1_TextChanged;
+            // 
+            // txtnombre
+            // 
+            txtnombre.Location = new Point(451, 165);
+            txtnombre.Name = "txtnombre";
+            txtnombre.Size = new Size(125, 27);
+            txtnombre.TabIndex = 1;
+            txtnombre.TextChanged += textBox2_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(109, 142);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Identificador";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(484, 142);
+            label2.Name = "label2";
+            label2.Size = new Size(64, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Nombre";
+            label2.Click += label2_Click;
+            // 
+            // btnInsertar
+            // 
+            btnInsertar.Location = new Point(289, 257);
+            btnInsertar.Name = "btnInsertar";
+            btnInsertar.Size = new Size(94, 29);
+            btnInsertar.TabIndex = 4;
+            btnInsertar.Text = "Insertar";
+            btnInsertar.UseVisualStyleBackColor = true;
+            btnInsertar.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnInsertar);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtnombre);
+            Controls.Add(txtId);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtId;
+        private TextBox txtnombre;
+        private Label label1;
+        private Label label2;
+        private Button btnInsertar;
+    }
+}
---
+++ ExamenJhonDaniel/Form1.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace ExamenJhonDaniel
+
+{
+    using System.CodeDom;
+    using System.Data.SqlClient;
+  
+
+    public partial class Form1 : Form
+    {
+
+        Sqlconnection conexion = new SqlConnection();
+        SqlCommand comando = new SqlCommand();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            conexion.ConnectionString = @"Data Source=MSI\SQLEXPRESS;Initial Catalog=examen;Integrated Security=True";
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                try
+                {
+
+                    comando.Connection = conexion;
+                    comando.CommandText = "insert into alumno(id,nombre) values(@id,@nombre)";
+                    conexion.Open();
+                    comando.Parameters.Clear();
+                    comando.Parameters.AddWithValue("@id", txtId.Text);
+                    comando.Parameters.AddWithValue("@nombre", txtNombre.Text);
+
+                    int NFilas = comando.ExecuteNonQuery();
+                    if (NFilas > 0)
+                    {
+                        MessageBox.Show("Se logro exitosamente.");
+
+                        txtId.Clear();
+                        txtNombre.Clear();
+                    }
+                }
+
+                catch (Exception ex)
+
+
+
+                {
+                    MessageBox.Show("FALLO LA CONSULTA: " + ex);
+
+                }
+
+                {
+                    finally
+                    conexion.Close();
+
+                }
+            }
+
+            private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
